dceab0ee13c70a82ac5a263514dc6a80e3042191
vijay9711 <vijaysm97@gmail.com>
2023-10-23 13:35:21 -0500

    home page

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-
+import './HomePAge.scss'
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       <div className="welcome-page-wrapper">
-        <h1 className="welcome-title">
-          welcome HomePage. Let's learn RedWood!
-        </h1>
+        <div className="row p-0 m-0 welcome-content">
+          <h1 className="welcome-title col-4">
+            Hello there,
+            <p>create a vision that stays with you always!</p>
+          </h1>
+        </div>
       </div>
     </>
   )
